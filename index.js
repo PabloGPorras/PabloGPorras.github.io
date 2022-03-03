cc2aeac47e96f016832152736174b89f50f81397
PabloGPorras <93367833+PabloGPorras@users.noreply.github.com>
2022-03-03 15:55:33 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from "React"
+import ReactDom from "react-dom"
+
+
+function Page() {
+  return (
+ <ol>
+    <li>reason 1</li>
+    <li>reason 2</li>
+ </ol>
+    
+  
+  
+  )
+}
+
+
+
+ReactDom.render(<h1>It's working!</h1>, document.getElementById("root"))
